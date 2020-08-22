75b9c0f1153a2578cf19358d593d06566c1627e5
prajwalmw <prajwalwaingankar@yahoo.in>
2020-08-22 19:00:33 +0530

    2nd Screen added as Kotlin.

---
+++ app/src/main/java/com/prajwal/prajwalwaingankar_cavista/Kotlin_Second_Screen.kt
@@ -0,0 +1,11 @@
+package com.prajwal.prajwalwaingankar_cavista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Kotlin_Second_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin__second__screen)
+    }
+}
