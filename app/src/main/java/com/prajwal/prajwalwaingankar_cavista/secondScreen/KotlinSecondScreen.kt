84d1e36685a99b6825cec98cc1d3a3c5b61e3949
prajwalmw <prajwalwaingankar@yahoo.in>
2020-08-25 15:55:47 +0530

    Back arrow is setup in toolbar.

--- app/src/main/java/com/prajwal/prajwalwaingankar_cavista/secondScreen/KotlinSecondScreen.kt
+++ app/src/main/java/com/prajwal/prajwalwaingankar_cavista/secondScreen/KotlinSecondScreen.kt
@@ -1,17 +1,17 @@
 package com.prajwal.prajwalwaingankar_cavista.secondScreen
 
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.view.View
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.bumptech.glide.Glide
 import com.prajwal.prajwalwaingankar_cavista.R
-import com.prajwal.prajwalwaingankar_cavista.firstScreen.ImageAdapter
 import com.prajwal.prajwalwaingankar_cavista.model.RoomDB.CommentModel
 import com.prajwal.prajwalwaingankar_cavista.model.RoomDB.Database
 import kotlin.concurrent.thread
@@ -21,6 +21,13 @@ class KotlinSecondScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlinsecondscreen)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextAppearance(this, R.style.ToolbarTheme)
+        toolbar.setTitleTextColor(Color.WHITE)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowTitleEnabled(true)
+
         val url: String = intent.extras?.getString("url") ?: ""
         val image: ImageView = findViewById(R.id.imageView)
         val editText: EditText = findViewById(R.id.editTextComment)
@@ -28,13 +35,8 @@ class KotlinSecondScreen : AppCompatActivity() {
 
         val commentModel = CommentModel()
         val database = Database.getInstance(applicationContext)
-
-//        val imageAdapter = ImageAdapter(this, imageUrlsList)
-//
         val uri = Uri.parse(url)
         Glide.with(this).load(uri).asBitmap().into(image)
-//
-//        image.setImageResource(imageAdapter.imageIds[position])
 
         button.setOnClickListener { view ->
             val commentString = editText.text.toString()
@@ -47,5 +49,16 @@ class KotlinSecondScreen : AppCompatActivity() {
         }
 
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == android.R.id.home)
+        {
+            finish()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
